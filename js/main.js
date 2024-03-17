88c57e46cde5dc86c4261995dc87e26d0f8bff22
lilitantonyan <lilit@helix.am>
2024-03-18 00:52:17 +0400

    add slide effect

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function (){
+    $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3', 'page4','page5'],
+        sectionsColor: ['white', 'white', 'white', 'white', 'white'],
+        navigation:false,
+        afterRender: function(){
+            $('#pp-nav').addClass('custom');
+        },
+        afterLoad: function(anchorLink, index){
+            if(index>1){
+                $('#pp-nav').removeClass('custom');
+            }else{
+                $('#pp-nav').addClass('custom');
+            }
+        }
+    });
+})
